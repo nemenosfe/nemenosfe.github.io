9b9dce08db992257262bc7d9bf6fed41f27ef4e5
Jaume Llado <jaume.llado.avellaneda@est.fib.upc.edu>
2016-05-25 14:40:28 +0200

    Routes retocades

--- js/routes.js
+++ js/routes.js
@@ -6,81 +6,82 @@ angular.module('joox-news', ['ngRoute'])
 
 		// route for the home page
 		.when('/', {
-			templateUrl : '../index.html',
-
+			templateUrl : 'index.html'
 		})
 
-		.when('/submissions', {
-			templateUrl: '../index.html',
-			controller: 'SubmissionCtrl',
+		.when('/submissions/new', {
+			templateUrl: 'submit.html',
+			controller: 'newSubmissionCtrl'
 		})
 
 		.when('/submissions/:id', {
-			templateUrl: '../submission.html',
-			controller: 'singleSubmissionCtrl',
+			templateUrl: 'submission.html',
+			controller: 'singleSubmissionCtrl'
 		})
 
+		//Cal? /submissions no té vista
+		/*.when('/submissions', {
+			templateUrl: 'index.html',
+			controller: 'submissionCtrl'
+		})*/
+
 		.when('/comments/:id', {
-			templateUrl: '../comment.html',
-			controller: 'commentCtrl',
+			templateUrl: 'comment.html',
+			controller: 'commentCtrl'
 		})
 
 		.when('/ask', {
-			templateUrl: '../ask.html',
-			controller: 'askCtrl',
+			templateUrl: 'ask.html',
+			controller: 'askCtrl'
 		})
 
 		.when('/threads/:id', {
-			templateUrl: '../threads.html',
-			controller: 'threadsCtrl',
+			templateUrl: 'threads.html',
+			controller: 'threadsCtrl'
 		})
 
-		.when('/submissions/:id/like', {
-			templateUrl: '../submission.html',
+		/*.when('/submissions/:id/like', {
+			templateUrl: 'submission.html',
 			controller: 'singleSubmissionCtrl', // Falta poder fer PUT
-		})
+		})*/
 
 		.when('/users/:id/submissions', {
-			templateUrl: '../submissions.html',
-			controller: 'userSubmissionsCtrl',
+			templateUrl: 'submissions.html',
+			controller: 'userSubmissionsCtrl'
 		})
 
-		.when('/comments', {
+		/*.when('/comments', {
 			templateUrl: '../index.html', //tinc dubtes perque es un post XD
 			controller: 'commentCtrl',
 		})
 
-
 		.when('/comments/:id/like', {
 			templateUrl: '../comment.html',
-			controller: 'commentCtrl', //Falta poder fer PUT
-		})
-
+			controller: 'commentCtrl' //Falta poder fer PUT
+		})*/
 
 		.when('/users/:id/comments', {
-			templateUrl: '../comments.html',
-			controller: 'userCommentsCtrl',
+			templateUrl: 'comments.html',
+			controller: 'userCommentsCtrl'
 		})
 
-
-		.when('/replies', {
+		/*.when('/replies', {
 			templateUrl: '../index.html',
-			controller: 'repliesCtrl', //Falta aquest controllador per fer el POST (Crear una reply)
-		})
+			controller: 'repliesCtrl' //Falta aquest controllador per fer el POST (Crear una reply)
+		})*/
 
 
-		.when('/replies/:id/like', {
+		/*.when('/replies/:id/like', {
 			templateUrl: '../index.html',
-			controller: 'replyCtrl', //Falta per fer el put
-		})
+			controller: 'replyCtrl' //Falta per fer el put
+		})*/
 
 		.when('/users/:id', {
-			templateUrl: '../user.html',
-			controller: 'userCtrl', // Per GET i PUT??
+			templateUrl: 'user.html',
+			controller: 'userCtrl' // Per GET i PUT??
 		})
 
-
-		.otherwise({ templateUrl: '../index.html' }) //falta per fer PUT
+		.otherwise({ templateUrl: '404.html' })
 
 	$locationProvider.html5Mode(true);
 
