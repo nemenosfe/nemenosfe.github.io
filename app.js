98910eec0bc5c3cae4b16c418843792bd09a3c53
Oscar Serrano <oscar.serrano@est.fib.upc.edu>
2016-05-26 17:27:11 +0200

    ocwncw

--- app.js
+++ app.js
@@ -2,53 +2,46 @@ angular.module('jooxAngular',['ngRoute', 'commentCtrl', 'submissionCtrl', 'userC
 
 .config(function($routeProvider, $httpProvider) {
 
-	$routeProvider
+	$httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
 
+	$routeProvider
 		.when('/', {
 			templateUrl : 'views/submissions.html',
 			controller: 'submissionCtrl'
 		})
-
 		.when('/submissions/new', {
 			templateUrl: 'views/submit.html',
 			controller: 'newSubmissionCtrl'
 		})
-
-		.when('submissions/:id', {
+		.when('/submissions/:id', {
 			templateUrl: 'views/submission.html',
 			controller: 'singleSubmissionCtrl'
 		})
-
 		.when('/comments/:id', {
 			templateUrl: 'views/comment.html',
 			controller: 'commentCtrl'
 		})
-
 		.when('/ask', {
 			templateUrl: 'views/ask.html',
 			controller: 'askCtrl'
 		})
-
 		.when('/threads/:id', {
 			templateUrl: 'views/threads.html',
 			controller: 'threadsCtrl'
 		})
-
 		.when('/users/:id/submissions', {
 			templateUrl: 'views/submissions.html',
 			controller: 'userSubmissionsCtrl'
 		})
-
 		.when('/users/:id/comments', {
 			templateUrl: 'views/comments.html',
 			controller: 'userCommentsCtrl'
 		})
-
 		.when('/users/:id', {
 			templateUrl: 'views/user.html',
 			controller: 'userCtrl' // Per GET i PUT??
 		})
-
 		.otherwise({ templateUrl: 'views/404.html' })
 
 	//$locationProvider.html5Mode(true);
