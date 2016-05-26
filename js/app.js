bdcc18fb719abc9d5654fd894f3689e1f319f0d6
Jaume Llado <jaume.llado.avellaneda@est.fib.upc.edu>
2016-05-26 10:37:36 +0200

    test

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('joox-angular',['joox-routes']);
+var app = angular.module('joox-angular',['ngRoute']);
 
 app.controller('commentCtrl', function($scope, $http, $routeParams) {
 
@@ -87,5 +87,93 @@ app.controller('userSubmissionsCtrl', function($http, $scope, $routeParams) {
 app.controller('userCommentsCtrl', function($http, $scope, $routeParams) {
 
     $scope.comments = $http.get('https://still-earth-13848.herokuapp.com/api/users/' + id + '/comments');
+
+})
+
+app.config(function($routeProvider, $locationProvider) {
+
+	$routeProvider
+
+		// route for the home page
+		.when('/', {
+			templateUrl : 'submissions.html',
+			controller: 'submissionCtrl'
+		})
+
+		.when('/submissions/new', {
+			templateUrl: 'submit.html',
+			controller: 'newSubmissionCtrl'
+		})
+
+		.when('/submissions/:id', {
+			templateUrl: 'submission.html',
+			controller: 'singleSubmissionCtrl'
+		})
+
+		//Cal? /submissions no té vista
+		/*.when('/submissions', {
+			templateUrl: 'index.html',
+			controller: 'submissionCtrl'
+		})*/
+
+		.when('/comments/:id', {
+			templateUrl: 'comment.html',
+			controller: 'commentCtrl'
+		})
+
+		.when('/ask', {
+			templateUrl: 'ask.html',
+			controller: 'askCtrl'
+		})
+
+		.when('/threads/:id', {
+			templateUrl: 'threads.html',
+			controller: 'threadsCtrl'
+		})
+
+		/*.when('/submissions/:id/like', {
+			templateUrl: 'submission.html',
+			controller: 'singleSubmissionCtrl', // Falta poder fer PUT
+		})*/
+
+		.when('/users/:id/submissions', {
+			templateUrl: 'submissions.html',
+			controller: 'userSubmissionsCtrl'
+		})
+
+		/*.when('/comments', {
+			templateUrl: '../index.html', //tinc dubtes perque es un post XD
+			controller: 'commentCtrl',
+		})
+
+		.when('/comments/:id/like', {
+			templateUrl: '../comment.html',
+			controller: 'commentCtrl' //Falta poder fer PUT
+		})*/
+
+		.when('/users/:id/comments', {
+			templateUrl: 'comments.html',
+			controller: 'userCommentsCtrl'
+		})
+
+		/*.when('/replies', {
+			templateUrl: '../index.html',
+			controller: 'repliesCtrl' //Falta aquest controllador per fer el POST (Crear una reply)
+		})*/
+
+
+		/*.when('/replies/:id/like', {
+			templateUrl: '../index.html',
+			controller: 'replyCtrl' //Falta per fer el put
+		})*/
+
+		.when('/users/:id', {
+			templateUrl: 'user.html',
+			controller: 'userCtrl' // Per GET i PUT??
+		})
+
+		.otherwise({ templateUrl: '404.html' })
+
+	$locationProvider.html5Mode(true);
 
 });
