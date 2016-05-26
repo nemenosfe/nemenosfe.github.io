fb72a4299c8729d160da55290d4340d540bb05c7
Oscar Serrano <oscar.serrano@est.fib.upc.edu>
2016-05-26 17:46:38 +0200

    PLEASE

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 angular.module('jooxAngular',['ngRoute', 'commentCtrl', 'submissionCtrl', 'userCtrl'])
 
-.config(function($routeProvider, $httpProvider) {
+.config(function($routeProvider, $locationProvider, $httpProvider) {
 
 	$httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
@@ -44,6 +44,6 @@ angular.module('jooxAngular',['ngRoute', 'commentCtrl', 'submissionCtrl', 'userC
 		})
 		.otherwise({ templateUrl: 'views/404.html' })
 
-	//$locationProvider.html5Mode(true);
+	$locationProvider.html5Mode(true);
 
 });
