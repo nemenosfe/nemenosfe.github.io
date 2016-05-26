55a2a22b445a6627327cfa391791210c5101e61a
Jaume Llado <jaume.llado.avellaneda@est.fib.upc.edu>
2016-05-26 12:38:02 +0200

    test submissions.html

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ app.controller('askCtrl', function($scope, $http) {
 
 app.controller('submissionCtrl', function($scope, $http) {
 
-    $scope.submissions = $http.get('https://joox-new-nemenosfe.c9users.io/api/submissions');
+    $scope.submissions = $http.get('https://still-earth-13848.herokuapp.com/api/submissions');
     $scope.text = "Prova per saber si funciona";
 
 })
