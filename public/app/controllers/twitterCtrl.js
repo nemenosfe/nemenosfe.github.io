f9c7f8b5c476c7119f6980bb174583c498b4b1ee
Oscar Serrano <oscar.serrano@est.fib.upc.edu>
2016-05-28 20:42:05 +0200

    ibcdbicibd

--- public/app/controllers/twitterCtrl.js
+++ public/app/controllers/twitterCtrl.js
@@ -30,12 +30,12 @@ angular.module('twitterCtrl', ['jooxAngular.services'])
             .success(function(data) {
                 console.log(data['api_key']);
                 $rootScope.token = data['api_key'];
-                $rootScope.userID = data['id'];
+                $rootScope.userid = data['id'];
                 $rootScope.connected = true;
                 $rootScope.userName = $scope.name;
                 console.log($rootScope.connected);
                 console.log($rootScope.userName);
-                console.log($rootScope.userID);
+                console.log($rootScope.userid);
             })
         })
     }
@@ -48,13 +48,13 @@ angular.module('twitterCtrl', ['jooxAngular.services'])
             $scope.$apply(function() {
                 $scope.connectedTwitter = false
                 $rootScope.token = null;
-                $rootScope.userID = null;
+                $rootScope.userid = null;
                 $rootScope.connected = false;
                 $rootScope.userName = null;
                 console.log($rootScope.token);
                 console.log($rootScope.connected);
                 console.log($rootScope.userName);
-                console.log($rootScope.userID);
+                console.log($rootScope.userid);
             })
         });
     }
