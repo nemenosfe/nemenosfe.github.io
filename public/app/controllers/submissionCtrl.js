be4f88f976872608ef0efa109a9c0d2f196b120f
Octavi <octavi.prats@est.fib.upc.edu>
2016-05-28 18:02:54 +0000

    csseando

--- public/app/controllers/submissionCtrl.js
+++ public/app/controllers/submissionCtrl.js
@@ -60,6 +60,35 @@ angular.module('submissionCtrl', [])
         $scope.size = Object.keys($scope.ask).length + 1;
     });
 
+    $scope.getTime = function(date) {
+        var d = new Date(date.substring(0,4),(date.substring(5,7))-1,date.substring(8,10),date.substring(11,13),date.substring(14,16),date.substring(17,19),date.substring(20,23));
+        var seconds = Math.floor((new Date() - d) / 1000);
+
+        var interval = Math.floor(seconds / 31536000);
+
+        if (interval > 1) {
+            return interval + " years";
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return interval + " months";
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + " days";
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + " hours";
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + " minutes";
+        }
+
+        return Math.floor(seconds) + " seconds";
+    };
+
 })
 
 .controller('singleSubmissionCtrl', function($scope, $http, $routeParams) {
