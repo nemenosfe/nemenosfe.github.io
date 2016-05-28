e709d9a8c62cb3633f7a0d8edaff0654c2ff319f
Oscar Serrano <oscar.serrano@est.fib.upc.edu>
2016-05-28 11:47:00 +0200

    yolo

--- public/app/controllers/submissionCtrl.js
+++ public/app/controllers/submissionCtrl.js
@@ -1,5 +1,3 @@
-var external = require("assets/js/calculodata.js");
-
 angular.module('submissionCtrl', [])
 
 .controller('submissionCtrl', function($scope, $http) {
@@ -33,10 +31,42 @@ angular.module('submissionCtrl', [])
 
     $scope.submission = {};
 
+
+    $scope.getTime = function(date) {
+        var d = new Date(date.substring(0,4),(date.substring(5,7))-1,date.substring(8,10),date.substring(11,13),date.substring(14,16),date.substring(17,19),date.substring(20,23));
+        console.log(date);
+        console.log(d);
+        var seconds = Math.floor((new Date() - d) / 1000);
+
+        var interval = Math.floor(seconds / 31536000);
+
+        if (interval > 1) {
+            return interval + " years";
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return interval + " months";
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + " days";
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + " hours";
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + " minutes";
+        }
+
+        return Math.floor(seconds) + " seconds";
+    };
+
     $http.get('https://still-earth-13848.herokuapp.com/api/submissions/' + $routeParams.id + '.json')
     .success(function(data) {
         $scope.submission = data;
-        $scope.correctData = getTime($scope.submission.created_at);
+        $scope.correctData = $scope.getTime($scope.submission.created_at);
     });
 
     $scope.likeSubmission = function() {
