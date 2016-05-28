2f829f21ac2beb27d7d78da573f98adb95c7b5f4
xavier <xavier.algarra@est.fib.upc.edu>
2016-05-28 10:52:41 +0200

    time

--- public/assets/js/calculodata.js
+++ public/assets/js/calculodata.js
@@ -2,6 +2,7 @@ function getTime(data) {
   var d = new Date(data.substring(0,4),(data.substring(5,7))-1,data.substring(8,10),data.substring(11,13),data.substring(14,16),data.substring(17,19),data.substring(20,23))
 
 
+
   console.log(data);
   console.log(d);
   var seconds = Math.floor((new Date() - d) / 1000);
@@ -9,31 +10,38 @@ function getTime(data) {
   var interval = Math.floor(seconds / 31536000);
 
   if (interval > 1) {
-      document.body.appendChild( interval + " years");
+
+    var textnode = document.createTextNode(interval + " years");         // Create a text node
+    document.body.appendChild(textnode);
       return
   }
   interval = Math.floor(seconds / 2592000);
   if (interval > 1) {
-      document.body.appendChild( interval + " months");
+
+    var textnode = document.createTextNode(interval + " months");         // Create a text node
+    document.body.appendChild(textnode);
       return
   }
   interval = Math.floor(seconds / 86400);
   if (interval > 1) {
-      document.body.appendChild( interval + " days");
+    var textnode = document.createTextNode(interval + " days");         // Create a text node
+    document.body.appendChild(textnode);
       return
   }
   interval = Math.floor(seconds / 3600);
   if (interval > 1) {
-      document.body.appendChild( interval + " hours");
+    var textnode = document.createTextNode(interval + " hours");         // Create a text node
+    document.body.appendChild(textnode);
       return
   }
   interval = Math.floor(seconds / 60);
   if (interval > 1) {
-      document.body.appendChild( interval + " minutes");
+    var textnode = document.createTextNode(interval + " minutes");         // Create a text node
+    document.body.appendChild(textnode);
       return
   }
 
   //document.body.appendChild(form);
-
-  document.body.appendChild( Math.floor(seconds) + " seconds")
+  var textnode = document.createTextNode(Math.floor(seconds) + " seconds");         // Create a text node
+  document.body.appendChild(textnode);
 }
