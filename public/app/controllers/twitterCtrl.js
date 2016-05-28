b99f46231e073230a51086468e6e950798c287b9
Jaume Llado <jaume.llado.avellaneda@est.fib.upc.edu>
2016-05-28 18:06:50 +0200

    login wip

--- public/app/controllers/twitterCtrl.js
+++ public/app/controllers/twitterCtrl.js
@@ -26,7 +26,7 @@ angular.module('twitterCtrl', ['jooxAngular.services'])
             console.log("Id: " + $scope.id);
             console.log("Name: " + $scope.name);
             var url = 'https://joox-new-nemenosfe.c9users.io/api/login';
-            var data = $httpParamSerializerJQLike({
+            var data = $.param({
                 uid: $scope.id,
                 name: $scope.name
             })
