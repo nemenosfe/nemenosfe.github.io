4673a0ee1a5854fbae37687793fdfd45104f7ed7
xavier <xavier.algarra@est.fib.upc.edu>
2016-05-28 10:38:24 +0200

    time

--- public/assets/js/calculodata.js
+++ public/assets/js/calculodata.js
@@ -1,33 +1,39 @@
 function getTime(data) {
+  var d = new Date(data.substring(0,4),(data.substring(5,7))-1,data.substring(8,10),data.substring(11,13),data.substring(14,16),data.substring(17,19),data.substring(20,23))
 
-    var d = new Date(data.substring(0,4),(data.substring(5,7))-1,data.substring(8,10),data.substring(11,13),data.substring(14,16),data.substring(17,19),data.substring(20,23))
 
+  console.log(data);
+  console.log(d);
+  var seconds = Math.floor((new Date() - d) / 1000);
 
-    console.log(data);
-    console.log(d);
-    var seconds = Math.floor((new Date() - d) / 1000);
+  var interval = Math.floor(seconds / 31536000);
 
-    var interval = Math.floor(seconds / 31536000);
+  if (interval > 1) {
+      document.getElementById("data").innerHTML+= ( interval + " years");
+      return
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+      document.getElementById("data").innerHTML+= ( interval + " months");
+      return
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+      document.getElementById("data").innerHTML+= ( interval + " days");
+      return
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+      document.getElementById("data").innerHTML+= ( interval + " hours");
+      return
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+      document.getElementById("data").innerHTML+= ( interval + " minutes");
+      return
+  }
 
-    if (interval > 1) {
-        return interval + " years";
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-        return interval + " months";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-        return interval + " days";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-        return interval + " minutes";
-    }
+  //document.body.appendChild(form);
 
-    document.getElementById("data").innerHTML+= ( Math.floor(seconds) + " seconds")
+  document.getElementById("data").innerHTML+= ( Math.floor(seconds) + " seconds")
 }
