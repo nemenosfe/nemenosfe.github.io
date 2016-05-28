b14ebc9285facb7951382430bf9f887c99dbaa03
xavier <xavier.algarra@est.fib.upc.edu>
2016-05-28 10:17:05 +0200

    time

--- public/assets/js/calculodata.js
+++ public/assets/js/calculodata.js
@@ -3,6 +3,8 @@ function getTime(data) {
     var d = new Date(data.substring(0,4),(data.substring(5,7))-1,data.substring(8,10),data.substring(11,13),data.substring(14,16),data.substring(17,19),data.substring(20,23))
 
 
+    console.log(data);
+    console.log(d);
     var seconds = Math.floor((new Date() - d) / 1000);
 
     var interval = Math.floor(seconds / 31536000);
