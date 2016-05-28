7c419a32058f18697668575412bb5ee782a2d3f7
Octavi <octavi.prats@est.fib.upc.edu>
2016-05-28 17:48:30 +0000

    cseando

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -21,6 +21,36 @@ angular.module('userCtrl', [])
             headers: {'X-Api-Key': 'W4cMakBP2LelZFjtEEzM0gtt'}
         });
     };
+    $scope.getTime = function(date) {
+        var d = new Date(date.substring(0,4),(date.substring(5,7))-1,date.substring(8,10),date.substring(11,13),date.substring(14,16),date.substring(17,19),date.substring(20,23));
+        console.log(date);
+        console.log(d);
+        var seconds = Math.floor((new Date() - d) / 1000);
+
+        var interval = Math.floor(seconds / 31536000);
+
+        if (interval > 1) {
+            return interval + " years";
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return interval + " months";
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + " days";
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + " hours";
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + " minutes";
+        }
+
+        return Math.floor(seconds) + " seconds";
+    };
 
 })
 
